03ab6aea6e1874c3188d4a886c47b6167b93226a
Leo Natan <lnatan@wix.com>
2018-05-20 00:56:45 +0300

    Remove change log generator
    Modify release version to advance the release version automatically

--- releaseVersion.sh
+++ releaseVersion.sh
@@ -1,44 +1,61 @@
 #!/bin/bash
 set -e
 
-if [ "$#" -ne 1 ]; then
-	echo -e >&2 "\033[1;31mIllegal number of parameters\033[0m"
-	echo -e >&2 "\033[1;31mreleaseVersion.sh <version>\033[0m"
-	exit -1
+# if [[ -n $(git status --porcelain) ]]; then
+#   echo -e >&2 "\033[1;31mCannot release version because there are unstaged changes:\033[0m"
+#   git status --short
+#   exit -2
+# fi
+#
+# if [[ -n $(git tag --contains $(git rev-parse --verify HEAD)) ]]; then
+#   echo -e >&2 "\033[1;31mThe latest commit is already contained in the following releases:\033[0m"
+#   git tag --contains $(git rev-parse --verify HEAD)
+#   exit -3
+# fi
+#
+# if [[ -n $(git log --branches --not --remotes) ]]; then
+#   echo -e "\033[1;34mPushing pending commits to git\033[0m"
+#   git push
+# fi
+
+CURRENT_VERSION=$(/usr/libexec/PlistBuddy LNPopupController/Info.plist -c "Print CFBundleShortVersionString")
+NEXT_VERSION=$(echo "$CURRENT_VERSION" | perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')
+
+echo -e "\033[1;34mUsing $NEXT_VERSION as release version\033[0m"
+
+echo -e "\033[1;34mCreating release notes\033[0m"
+
+RELEASE_NOTES_FILE=_tmp_release_notes.md
+
+rm -f "${RELEASE_NOTES_FILE}"
+touch "${RELEASE_NOTES_FILE}"
+open -Wn "${RELEASE_NOTES_FILE}"
+
+if ! [ -s "${RELEASE_NOTES_FILE}" ]; then
+  echo >&2 "\033[1;31mNo release notes provided, aborting.\033[0m"
+  rm -f "${RELEASE_NOTES_FILE}"
+  exit -1
 fi
 
-if [[ -n $(git status --porcelain) ]]; then
-	echo -e >&2 "\033[1;31mCannot release version because there are unstaged changes:\033[0m"
-	git status --short
-	exit -2
-fi
+#Escape user input in markdown to valid JSON string using PHP 🤦‍♂️ (https://stackoverflow.com/a/13466143/983912)
+RELEASENOTESCONTENTS=$(printf '%s' "$(<"${RELEASE_NOTES_FILE}")" | php -r 'echo json_encode(file_get_contents("php://stdin"));')
 
-if [[ -n $(git tag --contains $(git rev-parse --verify HEAD)) ]]; then
-	echo -e >&2 "\033[1;31mThe latest commit is already contained in the following releases:\033[0m"
-	git tag --contains $(git rev-parse --verify HEAD)
-	exit -3
-fi
+rm -f "${RELEASE_NOTES_FILE}"
 
-if [[ -n $(git log --branches --not --remotes) ]]; then
-  echo -e "\033[1;34mPushing pending commits to git\033[0m"
-	git push
-fi
+echo -e "\033[1;34mUpdating framework version\033[0m"
 
-echo -e "\033[1;34mUpdating CHANGELOG\033[0m"
+/usr/libexec/PlistBuddy LNPopupController/Info.plist -c "Set CFBundleShortVersionString $NEXT_VERSION" -c "Set CFBundleVersion 1"
 
-github_changelog_generator --future-release "$1" --no-verbose
-/usr/libexec/PlistBuddy LNPopupController/Info.plist -c "Set CFBundleShortVersionString $1" -c "Set CFBundleVersion 1"
-
-echo -e "\033[1;34mCommitting all changes to Git for release $1\033[0m"
+echo -e "\033[1;34mCommitting all changes to Git for release $NEXT_VERSION\033[0m"
 
 git add -A
-git commit -m "$1"
-git tag "$1"
+git commit -m "$NEXT_VERSION"
+git tag "$NEXT_VERSION"
 
 git push
 git push --tags
 
 echo -e "\033[1;34mCreating a GitHub release\033[0m"
 
-API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master", "name": "v%s", "body": "v%s", "draft": false, "prerelease": false}' "$1" "$1" "$1")
-RELEASE_ID=$(curl -s --data "$API_JSON" https://api.github.com/repos/LeoNatan/LNPopupController/releases?access_token=${GITHUB_RELEASES_TOKEN} | jq ".id")
+API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master", "name": "v%s", "body": %s, "draft": false, "prerelease": false}' "$NEXT_VERSION" "$NEXT_VERSION" "$RELEASENOTESCONTENTS")
+curl -s --data "$API_JSON" https://api.github.com/repos/LeoNatan/LNPopupController/releases?access_token=${GITHUB_RELEASES_TOKEN}
